29e562fb7e7b7d35296837ad1b27959b28c10d33
Trey Allen Gower <tagower@login3.frontera.tacc.utexas.edu>
2024-02-20 12:51:50 -0600

    remove duplicate data

---
+++ pt1/20/plot_uv.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+plotting u vs v for x =.5 and y = [0:1]
+"""
+
+__author__ = "Trey Gower, David Valenzano, Ty Zimmerman"
+
+def new_func():
+    """ {Main Description here}
+     
+     Args:
+
+     Returns: 
+    
+    """
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+url = 'https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt1/20/postProcessing/sample/0.5/line1_U.xy'
+with open

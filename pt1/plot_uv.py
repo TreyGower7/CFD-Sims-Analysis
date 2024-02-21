cf56e60e8b0c9c6292c47214e74f1e8063ec0bcc
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-02-21 12:03:14 -0600

    matrix not storing values right

--- pt1/plot_uv.py
+++ pt1/plot_uv.py
@@ -15,53 +15,62 @@ def get_data():
      Returns: 
     
     """
-    url = "https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt1/20/line1_U.xy"
-    response = requests.get(url)
-    data = []
-    if response.status_code == 200:
-        data = response.text.split('\n')  # Split the response into lines
-        for line in data:
-            print(line)  # Example: Print each line
-    else:
-        print("Failed to retrieve content. Status code:", response.status_code)
-    u = np.zeros([len(data),1])
-    v = np.zeros([len(data),1])
-    y = np.zeros([len(data),1])
-    x = np.zeros([len(data),1])
-    for i in range(len(data)-1):
-        u[i] = float((data[i].split())[3])
-        v[i] = float((data[i].split())[4])
-        x[i] = float((data[i].split())[0])
-        y[i] = float((data[i].split())[1])
-    #Weird but okay getting rid of random zero
-    u = u[:-1]
-    v = v[:-1]
-    x = x[:-1]
-    y = y[:-1]
+    ind=20;
+    for k in range(4):
+        url = "https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt1/" + str(ind) + "/line1_U.xy"
+        response = requests.get(url)
+        data = []
+        if response.status_code == 200:
+            data = response.text.split('\n')  # Split the response into lines
+            for line in data:
+                print('')  # Example: Print each line
+        else:
+            print("Failed to retrieve content. Status code:", response.status_code)
+        u = np.zeros([len(data),4])
+        v = np.zeros([len(data),4])
+        y = np.zeros([len(data),4])
+        x = np.zeros([len(data),4])
+        for i in range(len(data)-1):
+            u[i,k] = float((data[i].split())[3])
+            v[i,k] = float((data[i].split())[4])
+            x[i,k] = float((data[i].split())[0])
+            y[i,k] = float((data[i].split())[1])
+        ind *= 2
+        #Weird but okay getting rid of random zero
+    u = u[:-1, :]
+    v = v[:-1, :] 
+    x = x[:-1, :]
+    y = y[:-1, :]
+    #index to next grid size
     return u,v,x,y
 def plot(u,v,y):
     """Plotting for u vs v with x=.5
     """
-    plt.plot(y,u, '-b', y, v, '-r')
-    plt.title('u & v vs y, grid size 20, RE=10')
-    plt.xlabel('u')
-    plt.ylabel('y')
+    plt.plot(y, u, '-b', label='u vel')
+    plt.plot(y, v, '-r', label='v vel')
+    plt.legend(loc='upper left')  # Specify loc directly as a keyword argument
+    plt.title('velocity vs y, grid size 20, RE=10')
+    plt.xlabel('y')
+    plt.ylabel('Velocity')
     plt.grid()
     plt.show()
-    plt.savefig('u&v_vs_y.jpg')
+
     plt.plot(y,v, '-r')
-    plt.title('./v vs y, grid size 20, RE=10')
-    plt.xlabel('v')
-    plt.ylabel('y')
+    plt.title('v vs y, grid size 20, RE=10')
+    plt.xlabel('y')
+    plt.ylabel('v')
     plt.grid()
-    plt.savefig('./v_vs_y.jpg')
     plt.show()
 
 
 def main():
     """ Main entry point of the app """
     u,v,x,y = get_data()
-    plot(u,v,y)
+
+    print('u0')
+    print(u[1:20,1])
+
+    #plot(u,v,y)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """

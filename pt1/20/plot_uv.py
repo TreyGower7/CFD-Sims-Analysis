2cfcdbd819d77df3463f59419e706acac0f82c4e
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-02-20 23:39:24 -0600

    Response works

--- pt1/20/plot_uv.py
+++ pt1/20/plot_uv.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import requests
+import requests 
 """
 plotting u vs v for x =.5 and y = [0:1]
 """
@@ -17,14 +17,16 @@ def get_data():
     """
     url = "https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt1/20/line1_U.xy"
     response = requests.get(url)
-
+    #data = []
     if response.status_code == 200:
-       print("Content retrieved successfully:")
-       print(response.text)
+        data = response.text.split('\n')  # Split the response into lines
+        for line in data:
+            print(line)  # Example: Print each line
     else:
-       print("Failed to retrieve content. Status code:", response.status_code)
-#def plot_data():
-	
+        print("Failed to retrieve content. Status code:", response.status_code)
+
+    print(data)
+
 def main():
     """ Main entry point of the app """
     data = get_data()

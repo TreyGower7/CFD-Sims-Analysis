05657be81085055c97ed9233506ac9ce01d86e90
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-02-21 10:56:22 -0600

    plotting works

--- pt1/plot_uv.py
+++ pt1/plot_uv.py
@@ -15,9 +15,9 @@ def get_data():
      Returns: 
     
     """
-    url = "https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt2/RE10/line1_U.xy"
+    url = "https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/pt1/20/line1_U.xy"
     response = requests.get(url)
-    #data = []
+    data = []
     if response.status_code == 200:
         data = response.text.split('\n')  # Split the response into lines
         for line in data:
@@ -26,17 +26,42 @@ def get_data():
         print("Failed to retrieve content. Status code:", response.status_code)
     u = np.zeros([len(data),1])
     v = np.zeros([len(data),1])
+    y = np.zeros([len(data),1])
+    x = np.zeros([len(data),1])
     for i in range(len(data)-1):
         u[i] = float((data[i].split())[3])
         v[i] = float((data[i].split())[4])
-
+        x[i] = float((data[i].split())[0])
+        y[i] = float((data[i].split())[1])
     #Weird but okay getting rid of random zero
     u = u[:-1]
     v = v[:-1]
+    x = x[:-1]
+    y = y[:-1]
+    return u,v,x,y
+def plot(u,v,y):
+    """Plotting for u vs v with x=.5
+    """
+    plt.plot(y,u, '-b', y, v, '-r')
+    plt.title('u & v vs y, grid size 20, RE=10')
+    plt.xlabel('u')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+    plt.savefig('u&v_vs_y.jpg')
+    plt.plot(y,v, '-r')
+    plt.title('./v vs y, grid size 20, RE=10')
+    plt.xlabel('v')
+    plt.ylabel('y')
+    plt.grid()
+    plt.savefig('./v_vs_y.jpg')
+    plt.show()
+
+
 def main():
     """ Main entry point of the app """
-    data = get_data()
-
+    u,v,x,y = get_data()
+    plot(u,v,y)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """

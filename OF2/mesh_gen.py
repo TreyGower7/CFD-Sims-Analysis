c677b6a545ca250a074b01f40aee1467f49c6fd4
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-02-28 10:52:08 -0600

    Mesh_gen start

--- OF2/mesh_gen.py
+++ OF2/mesh_gen.py
@@ -1,1 +1,25 @@
 import numpy as np
+"""
+Mesh generation script for cylinder 
+"""
+
+__author__ = "Trey Gower, David Valenzano, Ty Zimmerman"
+
+def params():
+    """ 
+    enter paramters for cylinder
+    """
+    s_pole = np.array((0,-.5)) #North pole
+    n_pole = np.array((0,.5)) #North pole
+
+    n = input('Enter number of vertices: ') #enter number of vertices
+    blocks = n/2;
+    arcs = 
+
+def main():
+    """ Main entry point """
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()

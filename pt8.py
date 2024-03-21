dc330a7b30009f1c6d3b39c3e1bcb333cca9b681
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-03-21 13:32:32 -0500

    pt8 done

--- pt8.py
+++ pt8.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import requests 
 from matplotlib.cm import get_cmap
 import re
-from scipy.fft import fft, ifft
+from scipy.fft import fft, ifft, fftfreq
 
 """
 Module Docstring
@@ -14,59 +14,56 @@ def get_U():
     u1 = []
     t = []
 
-    mesh = 'A1'
-    line1 = f''
-    line2 = f''
+    mesh = 'B4'
+    line1 = f'https://raw.githubusercontent.com/TreyGower7/CFD_Code/main/OF2/mesh{mesh}/U'
     response = requests.get(line1)
-    response2 = requests.get(line2)
-
-    if response.status_code == 200 and response2.status_code == 200 and response3.status_code == 200:
+    if response.status_code == 200:
         lines1 = response.text.split('\n')  # Split the response into lines
-        lines2 = response2.text.split('\n')
     else:
         print("Failed to retrieve content. Status code:", response.status_code)
+
     pat = r'(\d+\.\d+)\s+\(([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\s+([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\s+([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\)\s+\(([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\s+([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\s+([-+]?\d+\.\d+(?:[eE][-+]?\d+)?)\)'
 
 # Print the first few lines as an example
     for i, line in enumerate(lines1):
         match = re.search(pat,line)
-        dim = match.groups()
-        if dim[0] >= 60:
-            u0.append(float(dim[1]))
-            t.append(float(dim[0]))
-
-            
-    for i, line in enumerate(lines2):
-            match = re.search(pat,line)
+        if match:
             dim = match.groups()
-            if dim[0] >= 60:
-                u1.append(float(dim[1]))
+            if float(dim[0]) >= 65:
+                u0.append(float(dim[1])) #probe 1 u vel
+                t.append(float(dim[0]))
+                u1.append(float(dim[4])) #probe 2 u vel
 
-def fast_fourier():
+    return u0,u1,t
+def fast_fourier(u0,u1,t):
+    N = len(t)
+    T = t[1] - t[0]  #uniform time steps
+    x = np.linspace(0.0, 0.5 / T, N//2)  # Adjusted x-axis range
+    y = u0  # Or any other data to perform FFT on
+    y2 = u1
+    yf = fft(y)
+    yf2 = fft(y2)
+    xf = fftfreq(N, T)[:N//2]
+    xf2 = fftfreq(N, T)[:N//2]
+    plt.plot(xf, 1.0/N * np.abs(yf[0:N//2]), label = 'Probe 1', color='r')
+    plt.plot(xf2, 1.0/N * np.abs(yf2[0:N//2]), label = 'Probe 2', color ='b')
+    plt.ylabel('Amplitude')
+    plt.xlabel(r'Frequency (Hz)')
+    plt.legend()
+    plt.grid()
+    plt.xlim([.06, 1])  # Adjusted x-axis limits
+    plt.ylim([0, .2])
+    plt.show()
+    
 
 # example y = fft(x)
  
-    return u1,v1,u2,v2,u3,v3
-
-def plotrt(vr1,vr2,vr3,vt1,vt2,vt3):
-    r = np.linspace(0,3,len(vt3))
-    plt.figure(1)
-    plt.plot(r,vt3, '-r',label='Probe 1')
-    #plt.plot(t,u1, '-b',label='Probe 2')
-    
-    plt.legend(loc='upper left')  # Specify loc directly as a keyword argument
-    plt.xlabel('t')
-    plt.ylabel('u/U')
-    plt.grid()
-
-    plt.show()
+    return 0
 
 def main():
-    u1,v1,u2,v2,u3,v3 =  get_U()
-
-    vr1,vr2,vr3,vt1,vt2,vt3 = convertrt(u1,v1,u2,v2,u3,v3)
-    plotrt(vr1,vr2,vr3,vt1,vt2,vt3)
-    
+    #Getting oscillitory u vels and time steps
+    u0,u1,t =  get_U()
+    fast_fourier(u0,u1,t)
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()

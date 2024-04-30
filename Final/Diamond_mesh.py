bd6cfcb67b0b342f557ff88881ff9bf9aae61cc1
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-04-30 11:00:07 -0500

    meshes

--- Final/Diamond_mesh.py
+++ Final/Diamond_mesh.py
@@ -13,25 +13,26 @@ def params():
     """ 
     enter paramters 
     """
-    L = 0
+    Lf = 2
+    Lw = 0
     H =0
     alpha_d =0
     #we will always have 32 vertices
     n = 32
 
     H = float(input('Enter H: '))
-    L = float(input('Enter L (Length of one side of the domain): '))
+    Lw = float(input('Enter Lw (Length of one side of the domain): '))
     alpha_d = float(input('Enter angle of attack in degrees: '))
     alpha_r = np.deg2rad(alpha_d)
 
-    return n, L, H, alpha_d
+    return n, Lf,Lw, H, alpha_r
 
 def generate_diamond(alpha):
     '''
     Generates diamond at various angles of attack
     '''
     #Diamond vertices
-    theta = np.deg2rad(5)
+    theta = np.deg2rad(3.433)
     v12 = np.array((-.5, 0))
     v13 = np.array((0,.5*np.tan(theta)))
     v14 = np.array((.5,0))
@@ -50,7 +51,7 @@ def generate_diamond(alpha):
     diamond = [v12,v13,v14,v15]
     return diamond
    
-def generate_other_verts(diamond, L, H):
+def generate_other_verts(diamond, Lf,Lw, H):
     '''
     generates all other vertices based on the diamond
     '''
@@ -64,18 +65,18 @@ def generate_other_verts(diamond, L, H):
     x15, y15 = v15
 
        # Define other vertices based on diamond
-    v0 = np.array((-L, -H))
+    v0 = np.array((-Lf, -H))
     v1 = np.array((x12, -H))
     v2 = np.array((x15, -H))
     v3 = np.array((x14, -H))
-    v4 = np.array((L, -H))
-    v5 = np.array((L, y14))
-    v6 = np.array((L, H))
+    v4 = np.array((Lw, -H))
+    v5 = np.array((Lw, y14))
+    v6 = np.array((Lw, H))
     v7 = np.array((x14, H))
     v8 = np.array((x13, H))
     v9 = np.array((x12, H))
-    v10 = np.array((-L, H))
-    v11 = np.array((-L, y12))
+    v10 = np.array((-Lf, H))
+    v11 = np.array((-Lf, y12))
 
     # Combine all vertices into a list
     vertices = [v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15]
@@ -97,10 +98,9 @@ def mesh_file():
     """ 
     saves the mesh in an openfoam readable format based on the example given
     """
-    n, L, H, alpha = params()
+    n, Lf,Lw, H, alpha = params()
     diamond = generate_diamond(alpha)
-    vertices = generate_other_verts(diamond, L, H)
-
+    vertices = generate_other_verts(diamond, Lf,Lw, H)
     formatted_string = "   ("
     i = 0
     for row in vertices:
@@ -108,25 +108,32 @@ def mesh_file():
         i += 1;
     formatted_string = formatted_string[:-2]  # Remove the extra "( " at the end
 
-    contents_to_modify = {'vert_template': '  (-1.0000000000000000e+01 -5.0000000000000000e+00 -5.0000000000000003e-02) // 0',}
+    contents_to_modify = {'vert_template': '  (-1.0000000000000000e+01 -5.0000000000000000e+00 -5.0000000000000003e-02) // 0', 'info': '// H = 5'}
 
     with open("/Users/treygower/Desktop/blockMeshDict.template", "r") as file:
         lines = file.readlines()
     
-    #inserts formatted string of vertices
+    meshletter = input("Enter a Letter to name the mesh with: ")
+    info = f'//Data for blockMeshDict_{meshletter}:\n//Alpha = {np.rad2deg(alpha)} Deg,\n//Turn Angle = 3.433 Deg,\n//Length of Fore = {Lf}m,\n//Length of Wake = {Lw}m,\n//Height = {H}m'
+#inserts formatted string of vertices
     for i, line in enumerate(lines):
         if contents_to_modify['vert_template'] in line:
             lines[i] = formatted_string
             break
-
-    meshletter = input("Enter a Letter to name the mesh with: ")
-# Write the modified lines back to the file
-    with open(f"/Users/treygower/Desktop/blockMeshDict_{meshletter}", "w") as file:
-        file.writelines(lines)
+    #inserts formatted string of vertices
+    for i, line in enumerate(lines):
+        if contents_to_modify['info'] in line:
+            lines[i] = info
+            break
 
     print("\n**************\n")
     print(f"Mesh blockMeshDict_{meshletter} has been generated\n")
-    print("**************\n")
+    print("**************\n\n")
+    print(f'Data for blockMeshDict_{meshletter}:\n Alpha = {np.rad2deg(alpha)} Deg, Turn Angle = 3.433 Deg, \n Length of Fore = {Lf}m,\n Length of Wake = {Lw}m,\n Height = {H}m')
+
+    # Write the modified lines back to the file
+    with open(f"/Users/treygower/Desktop/blockMeshDict_{meshletter}", "w") as file:
+        file.writelines(lines)
 
      # Plotting vertices
     plt.scatter(vertices[:15, 0], vertices[:15, 1])

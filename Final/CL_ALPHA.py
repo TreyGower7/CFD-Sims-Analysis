8e66f043c41a82a2b77f3f2a0025db94745b0eb0
Trey Gower <70235944+TreyGower7@users.noreply.github.com>
2024-05-03 21:44:18 -0500

    singlegraph

--- Final/CL_ALPHA.py
+++ Final/CL_ALPHA.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the data from the singleGraph file into a Pandas DataFrame
-data = pd.read_csv('/Users/treygower/Desktop/example/postProcessing/singleGraph/1/line_U.xy', delim_whitespace=True, header=None)
+data = pd.read_csv('/Users/treygower/Desktop/example/postProcessing/singleGraph/1/backbottom_p_T.xy', delim_whitespace=True, header=None)
 # Replace 'path/to/your/singleGraph_file' with the actual path to your file
 data.info()
 # Assign column names to the DataFrame for clarity
-data.columns = ['X', 'Y', 'Z', 'U_x', 'U_y', 'U_z']
+data.columns = ['X', 'Y', 'Z', 'p', 'T']
 data.info()
 # Plot the data
-plt.plot(data['U_x'], data['U_y'], label='')
+plt.plot(data['p'], data['X'], label='')
 #plt.plot(data[''], data['Value2'], label='Value2')
 plt.xlabel('X')
 plt.ylabel('Values')
